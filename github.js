88b89919eea9799e6bf27762856f2a83f68fff12
Ryan W. O'Hara <ryan@ryanohara.co>
2019-02-04 17:55:04 -0700

    Trigger when it should

--- github.js
+++ github.js
@@ -1,16 +1,18 @@
 
 function initGitHub(){
+        var href = '';
         $('a').each(function(key, element) {
-            if ($(element).attr('href').match('https:\/\/circleci.com\/')) {
-                $(element).attr('href', $(this).attr('href') + '&fullLogs=true');
+            href = $(element).attr('href');
+            if (href !== undefined && href.match('https:\/\/circleci.com\/') && !href.match('fullLogs=true')) {
+                $(element).attr('href', href + '&fullLogs=true');
             }
         });
 }
 
 $(document).ready(function(){
 
-	$(document).on('pjax:end', function (t) {
-		initGitHub();
-	});
+        $( "div.commit-ci-status summary" ).click(function (t) {
+            setTimeout(initGitHub, 500);
+        });
 
 });
